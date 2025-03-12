Working mesh regularization functionality: - Fixed bug which resulted in NaN gradients. The issue stemmed from a 0 division in some camera transformation done implicitely by the Pytorch3D rasterizer. This was easily fixed by inputting an eps parameter to the renderer. - The mesh regularization seems to be working. During developping I noticed that some artifacts occur when densification starts. This needs to be addressed. - Now the mesh renderer outputs the image, depth and normal map at the same time.

radfoam_model/mesh_utils.py
@@ -13,6 +13,9 @@ from pytorch3d.renderer import (
 )
 
 from pytorch3d.renderer import look_at_view_transform
+from pytorch3d.ops import interpolate_face_attributes
+
+
 def triangle_case1(tets, values, points, features, alpha_f, normal=True):
     '''one vertex is marked inside (resp. outside), three are outside (resp. inside).'''
     ins = tets[values<=0 if normal else values<0].repeat_interleave(3)
@@ -177,8 +180,37 @@ def nan_grad_hook_full(module, grad_input, grad_output):
             print(f"renderer NaN detected in gradients of {module.__class__.__name__} at indices: {nan_indices}")
             raise ValueError("Nan ground in renderer")
 
+def phong_normal_shading(meshes, fragments):
+    faces = meshes.faces_packed()  # (F, 3)
+    vertex_normals = meshes.verts_normals_packed()  # (V, 3)
+    faces_normals = vertex_normals[faces]
+    ones = torch.ones_like(fragments.bary_coords)
+    pixel_normals = interpolate_face_attributes(
+        fragments.pix_to_face, ones, faces_normals
+    )
+    return pixel_normals
+class MeshRendererWithDepthAndNormal(torch.nn.Module):
+    def __init__(self, rasterizer, shader):
+        super().__init__()
+        self.rasterizer = rasterizer
+        self.shader = shader
 
-def render_mesh(v,f,feat,data_handler,size=(64,64),idx=0):
+    def forward(self, meshes_world,return_only_image=False, **kwargs) -> torch.Tensor:
+        fragments = self.rasterizer(meshes_world, **kwargs)
+        images = self.shader(fragments, meshes_world, **kwargs)
+        if return_only_image:
+            return images
+        normals = phong_normal_shading(meshes_world,fragments)
+        norm = torch.norm(normals, dim=-1, keepdim=True)
+        v_normalized = normals / norm
+        return images, \
+               fragments.zbuf, \
+               v_normalized
+    
+
+
+
+def render_mesh(v,f,feat,data_handler,return_only_image=False,size=(64,64),idx=0):
     '''
     Render the mesh given extracted vertices v and faces f. 
     This function outputs an image
@@ -191,11 +223,7 @@ def render_mesh(v,f,feat,data_handler,size=(64,64),idx=0):
 
     textures = TexturesVertex(verts_features=features)
 
-    if verts.requires_grad:
-        verts.register_hook(lambda g: nan_grad_hook("verts",verts,g))
-
     mesh = Meshes(verts=verts, faces=faces, textures=textures)
-    # print('faces area',mesh.faces_areas_packed().shape,verts.shape)
     # Get the camera parameters
     # ground_truth_image, rotation, translation, height, width, fx, fy, cx, cy = get_camera_parameters(idx)
     ground_truth_image, rotation, translation, height, width, fx, fy, cx, cy = get_camera_parameters_from_data_handler(data_handler,idx,device)
@@ -216,8 +244,7 @@ def render_mesh(v,f,feat,data_handler,size=(64,64),idx=0):
     new_h,new_w = size
     raster_settings = RasterizationSettings(
         image_size=(new_h,new_w),
-        faces_per_pixel=10,
-        max_faces_per_bin=1e6
+        faces_per_pixel=10
     )
 
     # Define rasterizer
@@ -227,19 +254,39 @@ def render_mesh(v,f,feat,data_handler,size=(64,64),idx=0):
     shader = HardPhongShader(device=device, cameras=cameras, lights=lights)
 
     # Create the MeshRenderer
-    renderer = MeshRenderer(rasterizer=rasterizer, shader=shader)
+    renderer = MeshRendererWithDepthAndNormal(rasterizer=rasterizer, shader=shader)
     
     # Render the image
     square_side = min(height,width)
     reshape_gt = torchvision.transforms.CenterCrop((square_side,square_side))(ground_truth_image.permute(2,0,1))
     reshape_gt = torchvision.transforms.Resize((new_h,new_w))(reshape_gt).permute(1,2,0)
     
-    return reshape_gt,renderer(mesh)
+    
+    return reshape_gt,renderer(mesh,return_only_image=return_only_image,eps=1e-8)
 
-def mesh_render_plot(image,ground_truth_image,filename):
+def mesh_render_plot(image,depth,normal,ground_truth_image,filename):
     # Convert prediction image from RGBA to RGB
     pred_rgb = image[..., :3].cpu().detach().squeeze(0).numpy()
     pred_rgb = pred_rgb.clip(0, 1)  # Ensure valid range
+
+    # Plot depth
+    pred_depth = depth[:,:,:,:1].mean(-1,keepdim=True).cpu().detach().squeeze(0).numpy()
+
+    # Normal
+    pred_normal = normal[:,:,:,:1, :].mean(-2,keepdim=False).cpu().detach().squeeze(0).numpy()
+
+    # Normalize to range [0,1] for visualization
+    pred_normal_vis = (pred_normal + 1) / 2  
+    pred_normal_vis = np.clip(pred_normal_vis, 0, 1)  # Ensure values stay in range
+
+    # Normalize depth for visualization
+    depth_min, depth_max = pred_depth.min(), pred_depth.max()
+    if depth_max > depth_min:  # Avoid division by zero
+        pred_depth_vis = (pred_depth - depth_min) / (depth_max - depth_min)
+    else:
+        pred_depth_vis = np.zeros_like(pred_depth)
+
+    pred_depth_vis = np.clip(pred_depth_vis, 0, 1)
 
     # Ensure ground truth image is also in valid range
     if ground_truth_image is None:
@@ -254,23 +301,34 @@ def mesh_render_plot(image,ground_truth_image,filename):
 
     # Compute the error image (absolute difference)
     error_image = np.abs(pred_rgb - gt_rgb)
-    # Create a figure with three subplots
-    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+    # Create a figure with 2 rows: (1st row: 3 images, 2nd row: depth + empty slot)
+    fig, axes = plt.subplots(2, 3, figsize=(18, 12))
 
-    # Plot predicted image
-    axes[0].imshow(pred_rgb)
-    axes[0].set_title("Extraction")
-    axes[0].axis("off")
+    # First row
+    axes[0, 0].imshow(pred_rgb)
+    axes[0, 0].set_title("Extraction")
+    axes[0, 0].axis("off")
 
-    # Plot ground truth image
-    axes[1].imshow(gt_rgb)
-    axes[1].set_title("Ground Truth")
-    axes[1].axis("off")
+    axes[0, 1].imshow(gt_rgb)
+    axes[0, 1].set_title("Ground Truth")
+    axes[0, 1].axis("off")
 
-    # Plot error image
-    axes[2].imshow(error_image)
-    axes[2].set_title("Error Image")
-    axes[2].axis("off")
+    axes[0, 2].imshow(error_image)
+    axes[0, 2].set_title("Error Image")
+    axes[0, 2].axis("off")
+
+    # Second row
+    axes[1, 0].imshow(pred_depth_vis, cmap="viridis")
+    axes[1, 0].set_title("Depth Map")
+    axes[1, 0].axis("off")
+
+    # Empty slot for Normal Map (to be filled later)
+    axes[1, 1].imshow(pred_normal_vis)
+    axes[1, 1].set_title("Normal Map")
+    axes[1, 1].axis("off")
+
+    # Hide the last subplot (optional)
+    axes[1, 2].axis("off")
 
     plt.tight_layout()
     plt.savefig(filename)

radfoam_model/scene.py
@@ -8,7 +8,7 @@ import tqdm
 import radfoam
 from radfoam_model.render import TraceRays
 from radfoam_model.utils import *
-from radfoam_model.mesh_utils import marching_tetrahedra
+from radfoam_model.mesh_utils import marching_tetrahedra, render_mesh
 
 
 class RadFoamScene(torch.nn.Module):
@@ -506,6 +506,31 @@ class RadFoamScene(torch.nn.Module):
 
             self.densification_postfix(new_params)
             self.prune_points(prune_mask)
+
+    def collect_error_map_from_mesh(self,data_handler):
+
+        v, f, feat = self.get_mesh()
+        point_error_accum = torch.zeros_like(self.primal_points[..., 0:1])
+        
+        rgb_loss = nn.L1Loss(reduction="none")
+
+        for i in range(data_handler.c2ws.shape[0]):
+            gt_image_mesh, rgb_mesh = render_mesh(v,f,feat, data_handler,return_only_image=True,
+                                                                 size=(256,256), idx=i)
+            # White background
+            rgb_mesh_output = rgb_mesh[..., :3] #+ (1 - mesh_opacity)
+            
+            mesh_color_loss = rgb_loss(gt_image_mesh.cuda(),rgb_mesh_output)
+            mesh_color_loss.sum().backward(retain_graph=True)
+            point_error_accum += self.primal_points.grad.norm(
+                dim=-1, keepdim=True
+            ).detach()
+            torch.cuda.synchronize()
+
+            self.optimizer.zero_grad(set_to_none=True)
+            
+
+        return point_error_accum
 
     def collect_error_map(self, data_handler, white_bkg=True, downsample=2):
         rays, rgbs = data_handler.rays, data_handler.rgbs

train_along_mesh.py
@@ -18,9 +18,10 @@ from data_loader import DataHandler
 from configs import *
 from radfoam_model.scene import RadFoamScene
 from radfoam_model.utils import psnr
-from radfoam_model.mesh_utils import render_mesh, mesh_render_plot, nan_grad_hook
+from radfoam_model.mesh_utils import render_mesh, mesh_render_plot
 import radfoam
 
+SIZE=800
 
 seed = 42
 torch.random.manual_seed(seed)
@@ -59,7 +60,7 @@ def train(args, pipeline_args, model_args, optimizer_args, dataset_args):
         )
     )
     train_data_handler = DataHandler(
-        dataset_args, rays_per_batch=1_000_000, device=device
+        dataset_args, rays_per_batch=400_000, device=device
     )
     downsample = iter2downsample[0]
     train_data_handler.reload(split="train", downsample=downsample)
@@ -204,37 +205,33 @@ def train(args, pipeline_args, model_args, optimizer_args, dataset_args):
 
                 loss = color_loss.mean() + \
                     opacity_loss + w_depth * quant_loss
+                
                 # Mesh loss
                 primal_values = (model.get_primal_density().squeeze() - 5)
-                apply_mesh_loss = primal_values.max() > 0.0
+                apply_mesh_loss = primal_values.max() > 0.0 and i + 1 >= pipeline_args.mesh_regularization_from
                 if apply_mesh_loss:
-                # try:
                     v, f, feat = model.get_mesh()
                     random_training_index = np.random.randint(0,train_data_handler.c2ws.shape[0])
-                    gt_image_mesh, rgb_mesh = render_mesh(v,f,feat,
+                    gt_image_mesh, (rgb_mesh, depth_mesh_k_faces, normal_mesh_k_faces) = render_mesh(v,f,feat,
                                                           train_data_handler,
-                                                          size=(32,32),
+                                                          size=(SIZE,SIZE),
                                                           idx=random_training_index)
-                    
 
-                    if rgb_mesh.requires_grad:
-                        rgb_mesh.register_hook(lambda g: nan_grad_hook("rgb_mesh",rgb_mesh,g))
-                    # White background
-                    mesh_opacity = rgb_mesh[..., -1:]
-                    if pipeline_args.white_background:
-                        rgb_mesh_output = rgb_mesh[..., :3] #+ (1 - mesh_opacity)
-                    else:
-                        rgb_mesh_output = rgb_mesh[..., :3]
+                    mesh_opacity_loss = ((1 - rgb_mesh[..., -1:]) ** 2)
+                    mesh_color_loss = rgb_loss(gt_image_mesh.cuda(),rgb_mesh[..., :3])
 
-                    mesh_opacity_loss = ((1 - mesh_opacity) ** 2)
-                    mesh_color_loss = rgb_loss(gt_image_mesh.cuda(),rgb_mesh_output)
+                    # TODO: To apply depth and normal regularization we should:
+                    # - Change the radfoam training from batch to per image
+                    # - Import depth and normal estimation models to apply it to the gt image
+                    # - Make sure both the radiance and mesh branches use the same image 
+                    # depth_mesh = depth_mesh_k_faces[:,:,:,:1].mean(-1,keepdim=True)
                     
-                    mesh_color_report = mesh_color_loss.mean()
                     loss += mesh_color_loss.mean() + mesh_opacity_loss.mean()
-                # except:
+                
+                    mesh_color_report = mesh_color_loss.mean()
                 else:
                     mesh_color_report = torch.inf
-                ###############
+                    
                 model.optimizer.zero_grad(set_to_none=True)
 
                 # Hide latency of data loading behind the backward pass
@@ -280,14 +277,12 @@ def train(args, pipeline_args, model_args, optimizer_args, dataset_args):
                             primal_values = (model.get_primal_density().squeeze() - 5)
                             if primal_values.max() > 0:
                                 v, f, feat = model.get_mesh()
-                                gt_image_mesh, rgb_mesh = render_mesh(v,f,feat,
+                                gt_image_mesh, (rgb_mesh, depth_mesh, normal_mesh) = render_mesh(v,f,feat,
                                                                 train_data_handler,
-                                                                size=(128,128),
+                                                                size=(SIZE,SIZE),
                                                                 idx=0)
-                                mesh_render_plot(rgb_mesh,gt_image_mesh,filename=f"{out_dir}/mesh_vis/iteration_{i}.png")
-
-                # if iters_since_update >= triangulation_update_period:
-                #     model.apply_centroidal_voronoi_tessellation()
+                                mesh_render_plot(rgb_mesh,depth_mesh,normal_mesh,gt_image_mesh,
+                                                 filename=f"{out_dir}/mesh_vis/iteration_{i}.png")
 
                 if iters_since_update >= triangulation_update_period:
                     model.update_triangulation(incremental=True)
@@ -305,6 +300,8 @@ def train(args, pipeline_args, model_args, optimizer_args, dataset_args):
                     and model.primal_points.shape[0]
                     < 0.9 * model.num_final_points
                 ):
+                    # point_error_mesh = model.collect_error_map_from_mesh(train_data_handler) #TODO: Is this a good idea ?
+
                     point_error, point_contribution = model.collect_error_map(
                         train_data_handler, pipeline_args.white_background
                     )